6d446941b361b96387e0ec62ef8410f30feb03a6
Gunadi PS <gndsulistiawan@gmail.com>
2023-05-15 14:36:08 +0700

    menambahkan fungsi .env

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -10,7 +10,8 @@ const http = (token, fallback) => {
 
     const instance = axios.create({
         headers,
-        baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8888'
+        baseURL: import.meta.env.VITE_BACKEND_URL
+        // baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8888'
     })
 
     instance.interceptors.response.use((response)=>{
